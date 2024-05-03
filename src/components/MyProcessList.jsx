1e73f56844aea6701d1202d73b91c90904b60991
James Dalton <jdalton.rh.52@gmail.com>
2024-05-03 10:12:37 -0500

    more toaster

--- src/components/MyProcessList.jsx
+++ src/components/MyProcessList.jsx
@@ -4,6 +4,7 @@ import {FiltersContext} from "../context/filters.jsx";
 import {nextState} from "../services/myProcess.js";
 import {useMyProcessList} from "../hooks/useMyProcessList.js";
 import {MyProcess} from "./MyProcess.jsx";
+import {toast, Toaster} from "sonner";
 
 export function AvailableNextStates ({myProcess, workflow}) {
 
@@ -80,9 +81,24 @@ export function MyList ( {workflow}) {
     setSearch(event.target.value);
   }
 
+  function handleDelete(event) {
+     toast.error('TODO DELETE !!!');
+  }
+
+  function handleEdit(event) {
+     toast.error('TODO EDIT !!!');
+  }
+
+
   return (
 
       <Container >
+
+        <Toaster toastOptions={{
+          style: {
+            background: 'lightgray',
+          }
+        }}/>
 
         <MySpinner isLoading = {loading} />
 
@@ -170,20 +186,20 @@ export function MyList ( {workflow}) {
                   </td>
                   <td>
 
-                    <button type="button">
+                    <Button variant="outline-primary" type="button" >
 
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pen" viewBox="0 0 16 16">
+                      <svg onClick={handleEdit} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pen" viewBox="0 0 16 16">
                         <path
                             d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z"/>
                       </svg>
-                    </button>
+                    </Button>
 
-                    <button type="button">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3" viewBox="0 0 16 16">
+                    <Button variant="danger" type="button" >
+                      <svg onClick={handleDelete} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3" viewBox="0 0 16 16">
                         <path
                             d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
                       </svg>
-                    </button>
+                    </Button>
                   </td>
                 </tr>
             ))}
